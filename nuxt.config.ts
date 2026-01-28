84aa2f70baa0bf64f47507a6b76f8d4551d55b18
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2026-01-29 00:01:03 +0100

    Add base modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    typeCheck: false
+  },
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
 })
